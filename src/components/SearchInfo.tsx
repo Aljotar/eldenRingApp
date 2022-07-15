e0123fe5f1724d7ce78fd9a2cb976201b6adf35e
Jotar Alvaro <jotaralvaro@gmail.com>
2022-07-14 23:25:32 -0300

    genero componente de busqueda y hook debounce

---
+++ src/components/SearchInfo.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
+import Icon from 'react-native-vector-icons/Ionicons'
+import { useDebounced } from '../hooks/useDebounced'
+
+
+interface Props {
+    onDebounce: ( value: string ) => void;
+}
+
+export const SearchInfo = ( {onDebounce}: Props ) => {
+
+    const [ textValue, setTextValue] = useState('')
+
+    const debouncedValue =useDebounced( textValue, 1500 )
+
+
+    useEffect(() => {
+    
+        onDebounce(debouncedValue)
+
+    }, [debouncedValue])
+    
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.textBackground}>
+                <TextInput
+                    placeholder='Search'
+                    style={styles.textInput}
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    value={textValue}
+                    onChangeText={setTextValue}
+                />
+
+                <Icon
+                    name='search-outline'
+                    color='grey'
+                    size={30}
+                />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    textBackground: {
+        backgroundColor: '#F3F1F3',
+        borderRadius: 50,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15,
+        flexDirection: 'row',
+        paddingHorizontal: 20
+    },
+    textInput: {
+        flex: 1,
+        fontSize: 18,
+        top: 2
+    }
+})
--- src/screens/ClassListScreen.tsx
+++ src/screens/ClassListScreen.tsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import { ServerContainer } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react'
 import { FlatList, Image, Text, View } from 'react-native'
 import { ScrollView } from 'react-native';
 import { CardClass } from '../components/CardClass';
+import { SearchInfo } from '../components/SearchInfo';
 import { useClass } from '../hooks/useClass'
+import { ClassData } from '../interface/classInterfaces';
+
+
 
 export const ClassListScreen = () => {
 
     const { classList } = useClass();
 
+    const [term, setTerm] = useState('')
+
+    const [filterInfo, setFilterInfo] = useState<ClassData[]>([])
+
+
+    useEffect(() => {
+        
+        if ( term.length ===0 ){
+            return setFilterInfo(classList);
+
+        }
+
+        setFilterInfo(
+            classList.filter(info => info.name.toLocaleLowerCase()
+                            .includes( term.toLocaleLowerCase() ))
+        );
+
+    }, [term])
+    
     return (
         <ScrollView>
+
+            <SearchInfo 
+                onDebounce={ (value) => setTerm( value ) }
+            />
+
             <View style={{
                 alignItems: 'center'
             }}>
                 <FlatList
-                    data={classList}
+                    data={filterInfo}
                     keyExtractor={(clase) => clase.id}
                     numColumns={2}
                     showsVerticalScrollIndicator={false}
